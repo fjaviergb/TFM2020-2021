218655d0b24793ae234503395e910c556388a2fe
fjaviergb <fco.javier.gb1@gmail.com>
2020-10-06 18:33:43 +0200

    Phase 2 - getting in touch with iota-js

--- tt_iota/tx_reader.js
+++ tt_iota/tx_reader.js
@@ -1,16 +1,21 @@
 const Iota = require('@iota/core');
 const Extract = require('@iota/extract-json');
+const Converter = require('@iota/converter');
 
 const iota = Iota.composeAPI({
     provider: 'https://nodes.thetangle.org:443'
     });
 
-    const tailTransactionHash = '9GYNEDTAGYGS9RZFWZBVWZIXOUALMEMXKXULLMTCYXIEROMFS9AGKEZMRUHQV9UXCKGUTIDTEKNBA9999';
+const tailTransactionHash = 'WPFRNTOUDPAJZUOFNMRKHSIOXCDXMICC9QZUQQNCDYDKUUNZMESJJHFVVBQCEYZKDVZCBUVXRPIY99999';
 
 iota.getBundle(tailTransactionHash)
 .then(bundle => {
-    console.log(iota.getTrytes(bundle));
+    console.log(Converter.trytesToAscii(bundle[0]['signatureMessageFragment'].slice(0, -1)));
 })
 .catch(err => {
     console.error(err);
 });
+
+// iota.findTransactionObjects({addresses: addressesList})
+// .then(transactions => {console.log(transactions[0]);})
+// .catch(err => {console.log(err);});
