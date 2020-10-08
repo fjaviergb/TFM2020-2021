8899a0c6c4b681b3b0109bb9735b260c037db594
fjaviergb <fco.javier.gb1@gmail.com>
2020-10-08 13:47:27 +0200

    Probando cosas

---
+++ tt_iota/simulator/popup.js
@@ -0,0 +1,21 @@
+const url = 'localhost:3000';
+
+document.addEventListener('DOMContentLoaded', onDomLoad, false);
+
+function onDomLoad() {
+    console.log('Prueba1')
+    document.querySelector('button').addEventListener('click', onclick, false)
+
+    function onclick (){     
+        console.log('Prueba2')
+        let address = document.querySelector('input').value;
+        chrome.url.sendMessage(tabs[0].id, address, onload)
+           
+    }
+
+    function onload(res) {
+        console.log('Prueba3')
+        let area = document.querySelector('textarea')
+        area.innerHTML = data;
+    }
+}
