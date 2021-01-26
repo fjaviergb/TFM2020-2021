6cecc97fac18326f290b15b1de311ab09156f93d
fjaviergb <fco.javier.gb1@gmail.com>
2021-01-26 21:44:07 +0100

    Docker-compose revised

--- docker20212601/db_updator/cronpy.py
+++
@@ -1,8 +0,0 @@
-from crontab import CronTab
-import os
-
-print(os.environ['HOME'])
-cron = CronTab(user=None)
-job = cron.new(command='echo Hola Mundo')
-job.minute.every(1)
-cron.write()
--- docker20212601/db_updator/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-printenv | cat - /etc/cron.d/hello-cron > ~/hello-cron.tmp && mv ~/hello-cron.tmp /etc/cron.d/hello-cron
-
-cron -f
